92ed81a7ce384760c3becfb0bbf0dfa138c04c88
Oyvind Fanebust <oyvind.fanebust@gmail.com>
2013-06-21 14:02:23 +0200

    Tweaks to switch tests

--- Switcharoo.Tests/switching_features.cs
+++ Switcharoo.Tests/switching_features.cs
@@ -5,67 +5,75 @@ using Xunit;
 
 namespace Switcharoo.Tests
 {
-    public class checking_conditions : SwitcharooSpec
+    public class switching_features : SwitcharooSpec
     {
         [Fact]
-        public void feature_with_no_conditions_is_not_active()
+        public void feature_with_no_switches_is_not_active()
         {
             var feature = new Feature(Guid.NewGuid(), DateTime.Now, "Feature A");
+            
             var isActive = feature.IsActiveFor(new InstanceData());
 
             isActive.ShouldBeFalse();
         }
 
         [Fact]
-        public void feature_with_single_condition_is_active_when_instance_matches()
+        public void feature_with_single_switch_is_active_when_instance_matches()
         {
             var feature = new Feature(Guid.NewGuid(), DateTime.Now, "Feature A");
+            
             var @switch = new Switch("Dev");
             var condition = new SimpleCondition("environment", "dev");
             @switch.AddCondition(condition);
             feature.AddSwitch(@switch);
+            
             var instanceData = new InstanceData();
             instanceData.Add("environment", "dev");
+            
             var isActive = feature.IsActiveFor(instanceData);
 
             isActive.ShouldBeTrue();
         }
 
         [Fact]
-        public void feature_with_single_condition_is_not_active_when_instance_does_not_matches()
+        public void feature_with_single_switch_is_not_active_when_instance_does_not_matches()
         {
             var feature = new Feature(Guid.NewGuid(), DateTime.Now, "Feature A");
+            
             var @switch = new Switch("Dev");
             var condition = new SimpleCondition("environment", "dev");
             @switch.AddCondition(condition);
             feature.AddSwitch(@switch);
+            
             var instanceData = new InstanceData();
             instanceData.Add("environment", "prod");
+            
             var isActive = feature.IsActiveFor(instanceData);
 
             isActive.ShouldBeFalse();
         }
 
-        //No conditions -> true?
-        //One of multiple conditions -> false?
-
         [Fact]
-        public void feature_with_multiple_conditions_is_active_when_instance_matches_all_conditions()
+        public void feature_with_multiple_switches_is_active_when_instance_matches_at_least_one_switch()
         {
             var feature = new Feature(Guid.NewGuid(), DateTime.Now, "Feature A");
+            
             var @switch = new Switch("Dev");
-            var condition1 = new SimpleCondition("environment", "dev");
-            var condition2 = new SimpleCondition("computer_name", "dev_01");
-            @switch.AddCondition(condition1);
-            @switch.AddCondition(condition2);
+            var condition = new SimpleCondition("environment", "dev");
+            @switch.AddCondition(condition);
             feature.AddSwitch(@switch);
+            
+            var @switch2 = new Switch("Test");
+            var condition2 = new SimpleCondition("environment", "test");
+            @switch.AddCondition(condition2);
+            feature.AddSwitch(@switch2);
+            
             var instanceData = new InstanceData();
-            instanceData.Add("environment", "dev");
-            instanceData.Add("computer_name", "dev_01");
+            instanceData.Add("environment", "test");
+            
             var isActive = feature.IsActiveFor(instanceData);
 
             isActive.ShouldBeTrue();
         }
-
     }
 }
