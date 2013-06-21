513c2ff5f84d3b21be70516da733fd05cdf21e6a
Oyvind Fanebust <oyvind.fanebust@gmail.com>
2013-06-21 13:56:26 +0200

    Moved test support file

--- Switcharoo.Tests/Api/HttpClientFactory.cs
+++ Switcharoo.Tests/Api/HttpClientFactory.cs
@@ -3,7 +3,7 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Web.Http.SelfHost;
 
-namespace Switcharoo.Tests
+namespace Switcharoo.Tests.Api
 {
     public class HttpClientFactory
     {
@@ -14,7 +14,7 @@ namespace Switcharoo.Tests
             new Bootstrap().Configure(config);
             var server = new HttpSelfHostServer(config);
             var client = new HttpClient(server);
-            
+
             client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/hal+json"));
             try
             {
